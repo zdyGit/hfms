7f06e877fcf647bacc915c93e8217e320d202e25
zdyGit <1193009984@qq.com>
2017-02-06 22:15:06 +0800

    main account

--- python3_hfms/hfms/sys_frame/static/pagemanage.js
+++ python3_hfms/hfms/sys_frame/static/pagemanage.js
@@ -1,32 +1,16 @@
-var setting = {
-	view:{
-		selectedMulti:false
-	},
-	data:{
-		simpleData:{
-			enable:true,
-			idKey:"id",
-			pIdKey:"pid",
-			rootPid:0
-		}
-	},
-	callback:{
-		onDblClick:function(treeid,treenode){
-			alert("双击")
-		} 
-	}
-}
-
-var data = [
-	{id:1,pid:0,open:true,name:"root"},
-	{id:2,pid:1,name:"test"},
-	{id:3,pid:1,name:"test"},
-	{id:4,pid:1,name:"test"},
-	{id:5,pid:3,name:"test"},
-	{id:6,pid:3,name:"test"}
-]
-
 $(function(){
-	console.log("test")
-	$.fn.zTree.init($("#menutree"),setting,data)
+	$("#dg").datagrid({
+        	singleSelect:true,
+        	title:"所有账户",
+		fitColumns:false,
+		nowrap:false,
+		rowStyler:function(index,row){
+			if(index%2==1){
+				return "background-color:#FFF5EE;"
+			}
+			else{
+				return "background-color:#E0EEEE;"
+			}
+		}
+	})
 })
